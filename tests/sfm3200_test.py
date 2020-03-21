6abdc4d40d2f7aefd292407132b4139c8bcc066e
Reznic <reznic.tal@gmail.com>
2020-03-21 21:43:05 +0000

    Manual test scripts for drivers added

---
+++ tests/sfm3200_test.py
@@ -0,0 +1,12 @@
+from sfm3200_flow_sensor import Sfm3200
+import logging
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    sfm = Sfm3200()
+    flow = sfm.read_flow_slm()
+    print flow
+
+if __name__ == "__main__":
+    main()
