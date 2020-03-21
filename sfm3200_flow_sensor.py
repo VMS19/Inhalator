bf74956ab11164fb16c703df2332344ae40b8f83
Reznic <reznic.tal@gmail.com>
2020-03-21 10:04:10 +0200

    flow driver command writing looks complete

--- sfm3200_flow_sensor.py
+++ sfm3200_flow_sensor.py
@@ -1,41 +1,50 @@
-import smbus
+"""SFM3200 air flow sensor driver.
+
+The use of this driver requires pigpio deamon:
+'sudo pigpiod'
+(sudo apt-get install pigpio)
+"""
+import pigpio
 
 class Sfm3200:
+    I2C_BUS = 1
     I2C_ADDRESS = 0x40
     SCALE_FACTOR_FLOW = 120
     OFFSET_FLOW = 0x8000
-    START_MEASURE_CMD = 0x1000
-    READ_SERIAL_CMD = 0x31AE
-    SOFT_RST_CMD = 0x2000
+    START_MEASURE_CMD = b"\x10\x00"
+    SOFT_RST_CMD = b"\x20\x00"
 
     def __init__(self):
-        self._bus = smbus.SMBus(1)
+        self._pig = pigpio.pi()
+        self._dev = self._pig.i2c_open(self.I2C_BUS, self.I2C_ADDRESS)
+
         self._start_measure()
-        # Dummy read
+        # Dummy read - first read values are invalid
+        _ = self._pig.i2c_read_device(self._dev, 3)
 
-    def _read_byte(self):
-        return self._bus.read_byte(self.I2C_ADDRESS)
 
-    def _write_cmd(self, cmd):
-        cmd_high = cmd >> 8
-        cmd_low = cmd & 0xff
-        self._bus.write_byte(self.I2C_ADDRESS, cmd_high)
-        self._bus.write_byte(self.I2C_ADDRESS, cmd_low)
+    # Todo: Recognize nacks from slave, and call _start_measure after
+
+    # Todo: catch read/write fail errors. notify device not found
 
     def _start_measure(self):
-        self._write_cmd(self.START_MEASURE_CMD)
+        self._pig.i2c_write_device(self._dev, self.START_MEASURE_CMD)
 
     def soft_reset(self):
-        self._write_cmd(self.SOFT_RST_CMD)
+        self._pig.i2c_write_device(self._dev, self.SOFT_RST_CMD)
 
-    def read_pressure(self):
-        raw_value_high = self._read_byte()
-        raw_value_low = self._read_byte()
-        crc_value = self._read_byte()
-        raw_value = raw_value_high << 8 | raw_value_low;
-        # Calc & compare CRC
+    def read_pressure_slm(self):
+        read_size, data = self._pig.i2c_read_device(self._dev, 3)
+        if read_size == 3:
+            # Todo: combine 2 first byes, & little endian fix?
+            # Todo: Calc & compare CRC
+        elif read_size == 2:
+            # Todo: read without crc
+        elif  read_size == 0:
+            # Measurement not ready
+            pass
+
+        # Normalize flow to slm units
         flow = float(raw_value - self.OFFSET_FLOW) / self.SCALE_FACTOR_FLOW
         return flow
-
-
 
