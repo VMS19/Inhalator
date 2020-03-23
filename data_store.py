38590f3334134859617cce50057b23b3ca152ddc
rinslow <yonbruchim@gmail.com>
2020-03-23 15:23:54 +0200

    numpy removed
    Signed-off-by: rinslow <yonbruchim@gmail.com>

--- data_store.py
+++ data_store.py
@@ -1,5 +1,4 @@
 from queue import Queue
-import numpy as np
 
 
 class DataStore(object):
@@ -12,28 +11,20 @@ class DataStore(object):
         self.pressure_max_threshold = [15]  # mbar
         self.flow_min_threshold = [0.3] # Liter
         self.flow_max_threshold = [15]
-        self.flow_display_values = np.arange(0, 40, 1) * 0
-        self.pressure_display_values = np.arange(0, 40, 1)
-        self.x_axis = np.arange(0, 40, 1)
+        self.flow_display_values = [0] * 40
+        self.pressure_display_values = range(0, 40)
+        self.x_axis = range(0, 40)
         self.alerts = Queue(maxsize=10)
 
     def update_flow_values(self, new_value):
         if len(self.flow_display_values) == len(self.x_axis):
-            self.flow_display_values = \
-                np.append(np.delete(self.flow_display_values, 0), new_value)
-
-        else:
-            self.flow_display_values = np.append(self.flow_display_values,
-                                                 new_value)
+            self.flow_display_values.pop(0)
+        self.flow_display_values.append(new_value)
 
     def update_pressure_values(self, new_value):
         if len(self.pressure_display_values) == len(self.x_axis):
-            self.pressure_display_values = \
-                np.append(np.delete(self.pressure_display_values, 0), new_value)
-
-        else:
-            self.pressure_display_values = np.append(self.pressure_display_values,
-                                                     new_value)
+            self.pressure_display_values.pop(0)
+        self.pressure_display_values.append(new_value)
 
     def set_alert(self, alert):
         self.alerts.put(alert)
