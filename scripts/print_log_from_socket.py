3070bf692c20082dabcf4007281966d8878f0ff4
Yair Yigal <yigalyairn@gmail.com>
2020-03-26 20:37:00 +0200

    added logging into a file and timestamps

--- scripts/print_log_from_socket.py
+++ scripts/print_log_from_socket.py
@@ -1,5 +1,24 @@
 import socket
 import pickle
+import logging
+
+level = logging.DEBUG
+logger = logging.getLogger()
+logger.setLevel(level)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('/tmp/inhalator.log')
+fh.setLevel(level)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(level)
+# create formatter and add it to the handlers
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 PORT = 7777
 
@@ -9,5 +28,5 @@ sock.bind(('0.0.0.0', PORT))
 while True:
     raw_log = sock.recv(2 ** 16)
     log_meta = pickle.loads(raw_log[4:])  # Remove header
-    print(log_meta['msg'])
+    logger.debug(log_meta['msg'])
 
