3a5370b77c01785b6e2c7d8d07ec43bd1b2d7998
Avi Shukron <avraham.shukron@gmail.com>
2020-03-28 13:14:56 +0300

    Fixed unit tests
    Signed-off-by: Avi Shukron <avraham.shukron@gmail.com>

--- tests/unit/test_sanity.py
+++ tests/unit/test_sanity.py
@@ -1,15 +1,11 @@
 import pytest
 
-from unittest.mock import MagicMock, patch
-
 from algo import Sampler
 from data.data_store import DataStore
 from data.thresholds import (FlowThreshold, PressureThreshold,
                              RespiratoryRateThreshold, VolumeThreshold)
-from drivers.mocks.mock_pressure_sensor import MockPressureSensor
-from drivers.mocks.mock_air_flow_sensor import MockAirFlowSensor
-from graphics.graphs import FlowGraph, AirPressureGraph, BlankGraph
-from graphics.panes import CenterPane
+from drivers.mocks.sensor import MockSensor
+from drivers.driver_factory import DriverFactory
 
 
 @pytest.fixture
@@ -26,8 +22,8 @@ def store():
 
 
 def test_sampler_inserts_pressure_measurement_to_store(store):
-    flow_sensor = MockAirFlowSensor()
-    pressure_sensor = MockPressureSensor()
+    flow_sensor = MockSensor(DriverFactory.generate_mock_air_flow_data())
+    pressure_sensor = MockSensor(DriverFactory.generate_mock_pressure_data())
     sampler = Sampler(store, flow_sensor, pressure_sensor)
     assert store.pressure_measurements.qsize() == 0
     sampler.sampling_iteration()
@@ -37,13 +33,12 @@ def test_sampler_inserts_pressure_measurement_to_store(store):
 
 
 def test_sampler_alerts_when_pressure_exceeds_maximum(store):
-    flow_sensor = MockAirFlowSensor()
-    pressure_sensor = MockPressureSensor()
+    flow_sensor = MockSensor(DriverFactory.generate_mock_air_flow_data())
+    pressure_sensor = MockSensor(DriverFactory.generate_mock_pressure_data())
     sampler = Sampler(store, flow_sensor, pressure_sensor)
     assert len(store.alerts_queue) == 0
     sampler.sampling_iteration()
     assert len(store.alerts_queue) == 0
-
 
     store.pressure_threshold = PressureThreshold(0, 0)
     sampler.sampling_iteration()
@@ -52,27 +47,26 @@ def test_sampler_alerts_when_pressure_exceeds_maximum(store):
 
 
 def test_sampler_alerts_when_pressure_exceeds_minimum(store):
-    flow_sensor = MockAirFlowSensor()
-    pressure_sensor = MockPressureSensor()
+    flow_sensor = MockSensor(DriverFactory.generate_mock_air_flow_data())
+    pressure_sensor = MockSensor(DriverFactory.generate_mock_pressure_data())
     sampler = Sampler(store, flow_sensor, pressure_sensor)
     assert len(store.alerts_queue) == 0
     sampler.sampling_iteration()
     assert len(store.alerts_queue) == 0
-
 
     store.pressure_threshold = PressureThreshold(0, -100)
     sampler.sampling_iteration()
 
     assert len(store.alerts_queue) == 1
 
+
 def test_sampler_alerts_when_flow_exceeds_maximum(store):
-    flow_sensor = MockAirFlowSensor()
-    pressure_sensor = MockPressureSensor()
+    flow_sensor = MockSensor(DriverFactory.generate_mock_air_flow_data())
+    pressure_sensor = MockSensor(DriverFactory.generate_mock_pressure_data())
     sampler = Sampler(store, flow_sensor, pressure_sensor)
     assert len(store.alerts_queue) == 0
     sampler.sampling_iteration()
     assert len(store.alerts_queue) == 0
-
 
     store.pressure_threshold = FlowThreshold(0, 0)
     sampler.sampling_iteration()
@@ -81,13 +75,12 @@ def test_sampler_alerts_when_flow_exceeds_maximum(store):
 
 
 def test_sampler_alerts_when_flow_exceeds_minimum(store):
-    flow_sensor = MockAirFlowSensor()
-    pressure_sensor = MockPressureSensor()
+    flow_sensor = MockSensor(DriverFactory.generate_mock_air_flow_data())
+    pressure_sensor = MockSensor(DriverFactory.generate_mock_pressure_data())
     sampler = Sampler(store, flow_sensor, pressure_sensor)
     assert len(store.alerts_queue) == 0
     sampler.sampling_iteration()
     assert len(store.alerts_queue) == 0
-
 
     store.pressure_threshold = FlowThreshold(0, -100)
     sampler.sampling_iteration()
