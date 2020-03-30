48282251221265bedc12f6303124a7b8994b0dff
Reznic <reznic.tal@gmail.com>
2020-03-30 17:28:22 +0300

    wd now uses timer to sleep.

--- drivers/wd_driver.py
+++ drivers/wd_driver.py
@@ -1,18 +1,32 @@
 import time
+from threading import Timer
 
 import RPi.GPIO as GPIO
 
 
 class WdDriver(object):
-    WD_SIGNAL_LEN = 0.05
-    WD_GPIO = 18
+    WD_REFRESH_HZ = 2000
+    WD_REFRESH_SIGNAL_SEC = (1 / WD_REFRESH_HZ)
+    WD_GPIO = 5
+    FAULT_GPIO = 19
 
     def __init__(self):
         # Set WD GPIO
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.WD_GPIO, GPIO.OUT)
 
-    def arm_wd(self):
+        # Set FAULT leg as high
+        GPIO.setup(self.FAULT_GPIO, GPIO.OUT)
+        GPIO.output(self.FAULT_GPIO, GPIO.HIGH)
+        self.refresh_delay_timer = Timer(self.WD_REFRESH_SIGNAL_SEC,
+                                         self._pull_wd_down)
+
+    def _pull_wd_up(self):
         GPIO.output(self.WD_GPIO, GPIO.HIGH)
-        time.sleep(self.WD_SIGNAL_LEN)
+
+    def _pull_wd_down(self):
         GPIO.output(self.WD_GPIO, GPIO.LOW)
+
+    def arm_wd(self):
+        self._pull_wd_up()
+        self.refresh_delay_timer.start()
