788fcd6e25a4146e17f346433db3beb902d1d943
Asaf Avatichi <asaf.avatichi@gmail.com>
2020-04-02 17:41:09 +0300

    fix errors
    Signed-off-by: Asaf Avatichi <asaf.avatichi@gmail.com>

--- drivers/mux_i2c.py
+++ drivers/mux_i2c.py
@@ -10,7 +10,6 @@ log = logging.getLogger(__name__)
 
 
 class MuxI2C(object):
-
     I2C_BUS = 1
     I2C_ADDRESS = 0x70
 
@@ -19,7 +18,7 @@ class MuxI2C(object):
             self._pig = pigpio.pi()
         except pigpio.error as e:
             log.error("Could not init pigpio lib. Did you run 'sudo pigpiod'?")
-            raise PiGPIOInitError("pigpio library init error") from e
+            raise PiGPIOInitError("pigpio library init error")
 
         if self._pig is None:
             log.error("Could not init pigpio lib. Did you run 'sudo pigpiod'?")
@@ -29,12 +28,12 @@ class MuxI2C(object):
             self._dev = self._pig.i2c_open(self.I2C_BUS, self.I2C_ADDRESS)
         except AttributeError as e:
             log.error("Could not init pigpio lib. Did you run 'sudo pigpiod'?")
-            raise PiGPIOInitError("pigpio library init error") from e
+            raise PiGPIOInitError("pigpio library init error")
 
         except pigpio.error as e:
             log.error("Could not open i2c connection to flow sensor."
                       "Is it connected?")
-            raise I2CDeviceNotFoundError("i2c connection open failed") from e
+            raise I2CDeviceNotFoundError("i2c connection open failed")
 
     def switch_port(self, port):
         int_port = int(port)
@@ -45,10 +44,10 @@ class MuxI2C(object):
             # First, reset the switch
             self._pig.i2c_write_device(self._dev, 0x00)
             
-            self._pig.i2c_write_device(self._dev, str(0b1 << int(port)))
+            self._pig.i2c_write_device(self._dev, str(0b1 << int_port))
         except pigpio.error as e:
             log.error("Could not switch cmd to mux. "
                       "Is the mux connected?.")
-            raise I2CWriteError("i2c write failed") from e
+            raise I2CWriteError("i2c write failed")
 
         log.info("Switch to port {}".format(port))
