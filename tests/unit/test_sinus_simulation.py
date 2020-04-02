b4aaae561397e913c6008fa1833efec46e9b8845
rfire <rfire01@gmail.com>
2020-04-02 13:15:31 +0300

    mock vsm

--- tests/unit/test_sinus_simulation.py
+++ tests/unit/test_sinus_simulation.py
@@ -1,6 +1,10 @@
+import csv
+import os
+import time
+
 import pytest
 
-from algo import Sampler
+from algo import Sampler, VentilationStateMachine
 from data.measurements import Measurements
 from data.events import Events
 from data.configurations import Configurations
@@ -46,6 +50,33 @@ def test_sampler_alerts_when_pressure_exceeds_maximum(events, measurements, conf
     sampler = Sampler(measurements, events, flow_sensor, pressure_sensor, oxygen_a2d)
 
     for _ in range(1000):
+        time.sleep(0.001)
         sampler.sampling_iteration()
         print("pressure:", list(measurements.pressure_measurements.queue)[0])
         print("\tCurrent state:", sampler.vsm.current_state)
+
+
+@pytest.fixture
+def real_data():
+    this_dir = os.path.dirname(__file__)
+    with open(os.path.join(this_dir, "a.csv"), "r") as f:
+        reader = csv.DictReader(f)
+        t = [float(row['timestamp']) for row in reader]
+
+    with open(os.path.join(this_dir, "a.csv"), "r") as f:
+        reader = csv.DictReader(f)
+        v = [float(row['pressure']) for row in reader]
+    return t, v
+
+
+def test_correct_state_transitions(real_data):
+    t, v = real_data
+    vsm = VentilationStateMachine(Measurements(), Events())
+    for timestamp, pressure in zip(t, v):
+        vsm.update(
+            pressure_cmh2o=pressure,
+            flow_slm=0,
+            o2_saturation_percentage=0,
+            timestamp=timestamp)
+        print("pressure:", list(vsm._measurements.pressure_measurements.queue)[0])
+        print("\tCurrent state:", vsm.current_state)
