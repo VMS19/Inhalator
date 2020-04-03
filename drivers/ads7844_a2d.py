ada932ed24c89608569ddf04190c88a64b4bc8f9
Yonatan Schachter <yonatan.schachter@gmail.com>
2020-04-03 15:51:40 +0300

    added percent to voltage ratio
    Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>

--- drivers/ads7844_a2d.py
+++ drivers/ads7844_a2d.py
@@ -28,6 +28,7 @@ class Ads7844A2D(object):
     SECOND_READING_BIT_SHIFT = 3
     SAMPLE_CHANNELS = [0, 1] # channels to sample, 0 = oxygen, 1 = battery
     CHANNEL_MAP = [0, 4, 1, 5, 2, 6, 3, 7]
+    VOLTAGE_PERCENT_RATIO = 38.4 # from experiment
 
     def __init__(self):
         self._spi = spidev.SpiDev()
@@ -81,5 +82,4 @@ class Ads7844A2D(object):
     def read(self, input_mode=MODE_SGL, power_down_mode=PD_DISABLED):
         sample_res = [self._sample_a2d(channel, input_mode, power_down_mode)\
                 for channel in self.SAMPLE_CHANNELS]
-        return sample_res[0]
-
+        return sample_res[0] * self.VOLTAGE_PERCENT_RATIO
