14657fad709fe67e71bd96d2a59b5e1c4c3e3815
Avi Shukron <avraham.shukron@gmail.com>
2020-04-04 22:54:22 +0300

    WD Task: Re-implement in a more robust way
    The task now lives for the entire application duration, even if the
    application missed checking-in in time.
    
    Also the WD code is greatly simplified
    
    Signed-off-by: Avi Shukron <avraham.shukron@gmail.com>

--- wd_task.py
+++ wd_task.py
@@ -1,34 +1,34 @@
-from threading import Thread, Timer
+import time
 import logging
+from threading import Thread
 
 log = logging.getLogger(__name__)
 
 
 class WdTask(Thread):
     WD_TIMEOUT = 0.3  # 300 msec
-    START_TIMEOUT_SEC = 4
+    GRACE_TIME = 4  # We allow the application this much time to start.
 
     def __init__(self, wd, arm_wd_event):
         super(WdTask, self).__init__()
         self.daemon = True
         self.wd = wd
         self.arm_wd_event = arm_wd_event
+        self.log = logging.getLogger(self.__class__.__name__)
 
     def run(self):
-        wd_check_timer = Timer(self.START_TIMEOUT_SEC, self._wd_periodic_check)
-        wd_check_timer.start()
-
-    def _wd_periodic_check(self):
-        # Check if wd arm event was set
-        if self.arm_wd_event.isSet():
-            self.wd.arm_wd()
-            self.arm_wd_event.clear()
-
-            wd_check_timer = Timer(self.WD_TIMEOUT, self._wd_periodic_check)
-            wd_check_timer.start()
-
-        else:
-            # wd timeout has passed. will trigger HW watchdog
-            log.error("watchdog not refeshed!")
-
-
+        self.log.info(
+            f"WD Task started. Waiting {self.GRACE_TIME} seconds for"
+            f"application to start")
+        # Let the application the opportunity to start.
+        time.sleep(self.GRACE_TIME)
+        self.log.info("Grace time passed, arming the dog")
+        self.wd.arm()
+        while True:
+            time.sleep(self.WD_TIMEOUT)
+            if self.arm_wd_event.isSet():
+                self.log.debug("Refreshing WD")
+                self.wd.arm()
+                self.arm_wd_event.clear()
+            else:
+                self.log.warning("Application missed checking-in in time!")
