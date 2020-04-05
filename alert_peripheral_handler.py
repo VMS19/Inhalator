11f6a8d4085d0daa79b546dc5a58c76101a8bd51
Jonathan <yonbruchim@gmail.com>
2020-04-05 16:05:06 +0300

    added another module
    Signed-off-by: Jonathan <yonbruchim@gmail.com>

---
+++ alert_peripheral_handler.py
@@ -0,0 +1,27 @@
+from drivers.aux_sound import SoundViaAux
+from data.alerts import AlertCodes
+
+class AlertPeripheralHandler(object):
+
+    def __init__(self, events, drivers):
+        self.events = events
+        self.drivers = drivers
+        self.events.alerts_queue.pubsub.subscribe(self, self.on_new_alert)
+        self.events.mute_alerts.pubsub.subscribe(self, self.on_mute)
+        self.alert_driver = drivers.acquire_driver("alert")
+        self.sound_device = drivers.acquire_driver("aux")
+
+    def on_new_alert(self, alert):
+        if alert is not AlertCodes.OK:
+            self.alert_driver.alert_medical_condition_on()
+        else:
+            self.alert_driver.alert_medical_condition_off()
+            self.sound_device.stop()
+
+    def on_mute(self, mute):
+        if mute:
+            self.alert_driver.alert_buzzer_off()
+            self.sound_device.stop()
+        else:
+            self.alert_driver.alert_buzzer_on()
+            self.sound_device.start()
