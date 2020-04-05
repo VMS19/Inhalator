d8d47d2372de9450208e7bee394faaa2e2f61dcf
Jonathan <yonbruchim@gmail.com>
2020-04-05 17:04:35 +0300

    changed leds from high to low
    Signed-off-by: Jonathan <yonbruchim@gmail.com>

--- drivers/alert_driver.py
+++ drivers/alert_driver.py
@@ -12,11 +12,11 @@ class AlertDriver(object):
 
         # Set System fault GPIO
         GPIO.setup(self.SYSTEM_FAULT_GPIO, GPIO.OUT)
-        GPIO.output(self.SYSTEM_FAULT_GPIO, GPIO.LOW)
+        GPIO.output(self.SYSTEM_FAULT_GPIO, GPIO.HIGH)
 
         # Set medical contition GPIO
         GPIO.setup(self.MEDICAL_CONTITION_GPIO, GPIO.OUT)
-        GPIO.output(self.MEDICAL_CONTITION_GPIO, GPIO.LOW)
+        GPIO.output(self.MEDICAL_CONTITION_GPIO, GPIO.HIGH)
 
 		# Set buzzer GPIO
         GPIO.setup(self.FAULT_BUZZER_GPIO, GPIO.OUT)
@@ -24,19 +24,19 @@ class AlertDriver(object):
 
     def alert_system_fault_on(self):
         self.buzzer_on()
-        GPIO.output(self.SYSTEM_FAULT_GPIO, GPIO.HIGH)
+        GPIO.output(self.SYSTEM_FAULT_GPIO, GPIO.LOW)
 
     def alert_system_fault_off(self):
         self.buzzer_off()
-        GPIO.output(self.SYSTEM_FAULT_GPIO, GPIO.LOW)
+        GPIO.output(self.SYSTEM_FAULT_GPIO, GPIO.HIGH)
 
     def alert_medical_condition_on(self):
         self.alert_buzzer_on()
-        GPIO.output(self.MEDICAL_CONTITION_GPIO, GPIO.HIGH)
+        GPIO.output(self.MEDICAL_CONTITION_GPIO, GPIO.LOW)
 
     def alert_medical_condition_off(self):
         self.alert_buzzer_off()
-        GPIO.output(self.MEDICAL_CONTITION_GPIO, GPIO.LOW)
+        GPIO.output(self.MEDICAL_CONTITION_GPIO, GPIO.HIGH)
 
     def alert_buzzer_on(self):
         GPIO.output(self.FAULT_BUZZER_GPIO, GPIO.LOW)
