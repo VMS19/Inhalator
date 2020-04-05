beebe35cfe5142c157eb23bc20c206320f495f9c
Jonathan <yonbruchim@gmail.com>
2020-04-05 16:29:09 +0300

    changed location of mutealerts class
    Signed-off-by: Jonathan <yonbruchim@gmail.com>

--- data/alerts.py
+++ data/alerts.py
@@ -111,3 +111,21 @@ class AlertsQueue(object):
 
         self.last_alert = Alert(AlertCodes.OK)
         self.pubsub.publish(self.last_alert)
+
+class MuteAlerts(object):
+
+    def __init__(self):
+        self.pubsub = PublishSubscriber()
+        self._alerts_muted = False
+        self.mute_time = None
+
+    def mute_alerts(self, value=None):
+        if value is not None:
+            self._alerts_muted = value
+        else:
+            self._alerts_muted = not self._alerts_muted
+
+        self.pubsub.publish(self._alerts_muted)
+
+        if self._alerts_muted:
+            self.mute_time = time.time()
--- data/events.py
+++ data/events.py
@@ -1,6 +1,6 @@
 import logging
 
-from data.alerts import AlertsQueue
+from data.alerts import AlertsQueue, MuteAlerts
 import time
 from data.publisher_subscriber import PublishSubscriber
 
@@ -11,21 +11,3 @@ class Events(object):
     def __init__(self):
         self.alerts_queue = AlertsQueue()
         self.mute_alerts = MuteAlerts()
-
-class MuteAlerts(object):
-
-    def __init__(self):
-        self.pubsub = PublishSubscriber()
-        self._alerts_muted = False
-        self.mute_time = None
-
-    def mute_alerts(self, value=None):
-        if value is not None:
-            self._alerts_muted = value
-        else:
-            self._alerts_muted = not self._alerts_muted
-
-        self.pubsub.publish(self._alerts_muted)
-
-        if self._alerts_muted:
-            self.mute_time = time.time()
