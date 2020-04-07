205aa81f9e81748562a9a24691e2b272c5fb74e9
rinslow <yonbruchim@gmail.com>
2020-04-07 15:07:03 +0300

    close rtc i2c connection
    Signed-off-by: rinslow <yonbruchim@gmail.com>

--- scripts/set_rtc_time.py
+++ scripts/set_rtc_time.py
@@ -1,15 +1,22 @@
 import sys
-sys.path.append('..')
+import os
+sys.path.append(os.path.dirname('.'))
 
 from drivers.rv8523_rtc import Rv8523Rtc
 from datetime import datetime
 
 
 def main():
-    rtc = Rv8523Rtc()
-    cur_date = datetime.datetime.now()
-    print('setting rtc time as ', cur_date)
-    rtc.set_time(cur_date)
+    rtc = None
+
+    try:
+        rtc = Rv8523Rtc()
+        cur_date = datetime.datetime.now()
+        print('setting rtc time as ', cur_date)
+        rtc.set_time(cur_date)
+    finally:
+        if rtc is not None:
+            rtc.close()
 
 
 if __name__ == "__main__":
