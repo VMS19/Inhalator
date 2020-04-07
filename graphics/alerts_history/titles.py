d57b60f5bcc5fe49fb4f43525f5fc97e461aed87
rinslow <elkissdan@gmail.com>
2020-04-07 20:36:06 +0300

    Aligned description titles

--- graphics/alerts_history/titles.py
+++ graphics/alerts_history/titles.py
@@ -28,5 +28,5 @@ class AlertTitles(object):
     def render(self):
         self.frame.place(relx=0, rely=0, relwidth=0.85, relheight=0.15)
         self.index_label.place(relx=0, rely=0, relheight=1, relwidth=0.05)
-        self.time_label.place(relx=0.05, rely=0, relheight=1, relwidth=0.25)
-        self.description_label.place(relx=0.3, rely=0, relheight=1, relwidth=0.7)
+        self.time_label.place(relx=0.05, rely=0, relheight=1, relwidth=0.3)
+        self.description_label.place(relx=0.35, rely=0, relheight=1, relwidth=0.65)
