e97cedc79bd32dbc33fc10f2e4ef5e0883d0f47f
rinslow <yonbruchim@gmail.com>
2020-04-07 14:44:40 +0300

    changed script to recive IP argument
    Signed-off-by: rinslow <yonbruchim@gmail.com>

--- scripts/set_rasp_date.sh
+++ scripts/set_rasp_date.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Set date on remote machine
-sshpass -p 'raspberry' ssh pi@1.1.1.1 sudo date -s @`( date -u +"%s" )`
+IP_ADDR=$1
+if [[ $# -eq 0 ]]; then
+	echo "No ip supplied"
+else
+	# Set date on remote machine
+	sshpass -p 'raspberry' ssh pi@$IP_ADDR sudo date -s @`( date -u +"%s" )`
 
-# Set rtc time
-sshpass -p 'raspberry' ssh pi@1.1.1.1 '~/Inhalator/scripts/set_rtc_time.py'
+	# Set rtc time
+	sshpass -p 'raspberry' ssh pi@$IP_ADDR '~/Inhalator/scripts/set_rtc_time.py'
+fi
