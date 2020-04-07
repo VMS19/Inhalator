52b2df3e4acb29cb166b83bb1634329ac277b390
rinslow <yonbruchim@gmail.com>
2020-04-08 00:14:57 +0300

    fixed
    Signed-off-by: rinslow <yonbruchim@gmail.com>

--- scripts/set_rasp_date.sh
+++ scripts/set_rasp_date.sh
@@ -5,8 +5,8 @@ if [[ $# -eq 0 ]]; then
     echo "No ip supplied"
 else
     # Set date on remote machine
-    sshpass -p 'raspberry' ssh pi@$IP_ADDR sudo date -s @`( date -u +"%s" )`
+    sshpass -p 'raspberry' ssh pi@$IP_ADDR "sudo date -s '$(date --iso-8601=seconds)'"
 
     # Set rtc time
-    sshpass -p 'raspberry' ssh pi@$IP_ADDR '/usr/bin/python3 ~/Inhalator/scripts/set_rtc_time.py'
+    sshpass -p 'raspberry' ssh pi@$IP_ADDR 'PYTHONPATH=~/Inhalator ~/Inhalator/.inhalator_env/bin/python ~/Inhalator/scripts/set_rtc_time.py'
 fi
