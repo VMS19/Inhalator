139549872060e4b1423a6f83305ae8f0d219f8ac
Avatichi <asaf@avatichi.com>
2020-04-13 00:25:32 +0300

    hopefully this is the right way to fix it
    Signed-off-by: Avatichi <asaf@avatichi.com>

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ class Application(object):
         return cls.__instance
 
     def __init__(self, measurements, events, arm_wd_event, drivers, sampler,
-                 simulation=False, fps=25, sample_rate=22):
+                 simulation=False, fps=15, sample_rate=90):
         self.should_run = True
         self.drivers = drivers
         self.arm_wd_event = arm_wd_event
@@ -33,14 +33,15 @@ class Application(object):
         self.simulation = simulation
         self.frame_interval = 1 / fps
         self.sample_interval = 1 / sample_rate
+        self.last_sample_update_ts = 0
+        self.last_gui_update_ts = 0
         self.root = Tk()
         self.theme = Theme.choose_theme()  # TODO: Make this configurable
         self.root.protocol("WM_DELETE_WINDOW", self.exit)  # Catches Alt-F4
         self.root.title("Inhalator")
         self.root.geometry('800x480')
         self.root.attributes("-fullscreen", True)
-        self.last_render_ts = 0
-        self.last_sample_ts = 0
+
 
         if os.uname()[1] == 'raspberrypi':
             # on production we don't want to see the ugly cursor
@@ -70,29 +71,23 @@ class Application(object):
         self.root.update()
         self.root.update_idletasks()
         self.master_frame.update()
-        self.last_render_ts = time.time()
 
     def sample(self):
         self.sampler.sampling_iteration()
-        self.last_sample_ts = time.time()
-
-    @property
-    def next_render(self):
-        return self.frame_interval - (time.time() - self.last_render_ts)
-
-    @property
-    def next_sample(self):
-        return self.sample_interval - (time.time() - self.last_sample_ts)
 
     def run(self):
         self.render()
         while self.should_run:
             try:
-                if self.next_sample > 0:
-                    time.sleep(max(self.next_sample, 0))
-                self.sample()
-                if self.next_render <= 0:
+                time_now = time.time()
+                if (time_now - self.last_gui_update_ts) >= self.frame_interval:
                     self.gui_update()
+                    self.last_gui_update_ts = time_now
+
+                if (time_now - self.last_sample_update_ts) >= self.sample_interval:
+                    self.sample()
+                    self.last_sample_update_ts = time_now
+
                 self.arm_wd_event.set()
             except KeyboardInterrupt:
                 break
