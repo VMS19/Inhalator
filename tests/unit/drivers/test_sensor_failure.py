8cfcc5fe4008e56f5def8a55c47dcbc9877caa59
Reznic <reznic.tal@gmail.com>
2020-04-15 14:08:43 +0300

    work in progress

--- tests/unit/drivers/test_sensor_failure.py
+++ tests/unit/drivers/test_sensor_failure.py
@@ -4,11 +4,23 @@ import pytest
 
 from algo import Sampler
 from data.alerts import AlertCodes
-from data.configurations import Configurations
 from data.events import Events
 from data.measurements import Measurements
 from drivers.driver_factory import DriverFactory
 from errors import UnavailableMeasurmentError
+
+
+df = DriverFactory(simulation_mode=True, simulation_data='sinus')
+flow = df.acquire_driver('flow')
+pressure = df.acquire_driver('pressure')
+a2d = df.acquire_driver('a2d')
+timer = df.acquire_driver('timer')
+
+flow.read = MagicMock(return_value=0)
+pressure.read = MagicMock(return_value=0)
+a2d.read_oxygen = MagicMock(return_value=0)
+a2d.read_battery_percentage = MagicMock(return_value=0)
+a2d.read_battery_existence = MagicMock(return_value=0)
 
 
 @pytest.fixture
@@ -23,30 +35,29 @@ def measurements():
 
 @pytest.fixture
 def drivers():
-    df = DriverFactory(simulation_mode=True, simulation_data='sinus')
-    flow = df.acquire_driver('flow')
-    pressure = df.acquire_driver('pressure')
-    a2d = df.acquire_driver('a2d')
-    timer = df.acquire_driver('timer')
-    return flow, pressure, a2d, timer
+    yield "setup"
+
+    flow.read = MagicMock(return_value=0)
+    pressure.read = MagicMock(return_value=0)
+    a2d.read_oxygen = MagicMock(return_value=0)
+    a2d.read_battery_percentage = MagicMock(return_value=0)
+    a2d.read_battery_existence = MagicMock(return_value=0)
 
 
+@pytest.mark.parametrize('read_method',
+                         [flow.read,
+                          pressure.read,
+                          a2d.read_oxygen,
+                          a2d.read_battery_percentage,
+                          a2d.read_battery_existence])
 def test_unavailable_measurement_not_crashing(events, measurements,
-                                                   drivers):
+                                              drivers, read_method):
+    print(f"running {drivers}")
     crash_msg = "Application did not handle correctly " \
                 "temporary unavailable sensor measurement, and crashed"
     measurement = 0
-    flow, pressure, a2d, timer = drivers
 
-    flow.read = MagicMock(return_value=measurement,
-                          side_effect=UnavailableMeasurmentError(crash_msg))
-    pressure.read = MagicMock(return_value=measurement,
-                              side_effect=UnavailableMeasurmentError(crash_msg))
-    a2d.read_oxygen = MagicMock(return_value=measurement,
-                            side_effect=UnavailableMeasurmentError(crash_msg))
-    a2d.read_battery_percentage = MagicMock(return_value=measurement,
-                            side_effect=UnavailableMeasurmentError(crash_msg))
-    a2d.read_battery_existence = MagicMock(return_value=measurement,
+    read_method.configure_mock(return_value=measurement,
                             side_effect=UnavailableMeasurmentError(crash_msg))
 
     sampler = Sampler(measurements=measurements, events=events,
@@ -56,19 +67,22 @@ def test_unavailable_measurement_not_crashing(events, measurements,
     sampler.sampling_iteration()
     # If exception is not raised, test passed
 
-def test_sampling_exception_raises_alert(events, measurements, drivers):
-    flow, pressure, a2d, timer = drivers
 
-    flow.read = MagicMock(return_value=3,
-                          side_effect=Exception(""))
-    pressure.read = MagicMock(return_value=3,
-                              side_effect=Exception(""))
-    a2d.read_oxygen = MagicMock(return_value=3,
-                                side_effect=Exception(""))
-    a2d.read_battery_percentage = MagicMock(return_value=3,
-                                side_effect=Exception(""))
-    a2d.read_battery_existence = MagicMock(return_value=3,
-                                side_effect=Exception(""))
+@pytest.mark.skip(reason="fuck")
+@pytest.mark.parametrize('read_method,expected_alert',
+                         [(flow.read, AlertCodes.FLOW_SENSOR_ERROR),
+                          (pressure.read, AlertCodes.PRESSURE_SENSOR_ERROR),
+                          (a2d.read_oxygen, AlertCodes.OXYGEN_SENSOR_ERROR),
+                          (a2d.read_battery_percentage, AlertCodes.NO_BATTERY),
+                          (a2d.read_battery_existence, AlertCodes.NO_BATTERY)])
+def test_sampling_error_raises_alert(events, measurements, read_method,
+                                         expected_alert):
+    crash_msg = "Application did not handle correctly " \
+                "sensor read failure, and crashed"
+    measurement = 0
+
+    read_method = MagicMock(return_value=measurement,
+                            side_effect=Exception(crash_msg))
 
     sampler = Sampler(measurements=measurements, events=events,
                       flow_sensor=flow, pressure_sensor=pressure, a2d=a2d,
@@ -78,5 +92,6 @@ def test_sampling_exception_raises_alert(events, measurements, drivers):
 
     sampler.sampling_iteration()
 
-    #assert any(alert == AlertCodes.OXYGEN_LOW for alert in
-    #           events.alerts_queue.queue.queue)
+    assert any(alert == expected_alert for alert in
+               events.alerts_queue.queue.queue), \
+        "Sensor error did not raise correct alert"
