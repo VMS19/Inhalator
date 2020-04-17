285ccb524c27053f4ce8ede1cacc335a1cc50b68
rfire <rfire01@gmail.com>
2020-04-17 16:07:39 +0300

    updated tests

--- tests/unit/test_grace_time.py
+++ tests/unit/test_grace_time.py
@@ -1,4 +1,4 @@
-import time
+from unittest.mock import patch
 
 import pytest
 
@@ -42,30 +42,60 @@ def measurements():
 
 @pytest.fixture
 def events():
-    return Events()
+    e = Events()
+    e.alerts_queue.initial_uptime = 0
+    return e
 
 
-def test_grace_time_alerts(events, measurements, config, driver_factory):
+@pytest.fixture
+def sampler(events, measurements, driver_factory):
     flow_sensor = driver_factory.acquire_driver("flow")
     pressure_sensor = driver_factory.acquire_driver("pressure")
     a2d = driver_factory.acquire_driver("a2d")
     timer = driver_factory.acquire_driver("timer")
-    sampler = Sampler(measurements, events, flow_sensor, pressure_sensor,
-                      a2d, timer, average_window=1)
+    return Sampler(measurements, events, flow_sensor, pressure_sensor,
+                   a2d, timer, average_window=1)
 
-    config.volume_range = VolumeRange(HIGH_THRESHOLD, HIGH_THRESHOLD)
 
-    current_time = time.time()
-    while time.time() - current_time < GRACE_TIME:
-        sampler.sampling_iteration()
+@patch("data.alerts.uptime")
+def test_grace_time_alerts(mocked_uptime, events, config, sampler):
+    """Check no alerts in grace time, and alert received after."""
+    mocked_uptime.return_value = GRACE_TIME / 2
 
+    config.pressure_range = PressureRange(HIGH_THRESHOLD, HIGH_THRESHOLD)
+
+    sampler.sampling_iteration()
     assert len(events.alerts_queue) == 0, "shouldn't get any alerts during grace time"
 
-    current_time = time.time()
-    while time.time() - current_time < GRACE_TIME:
-        sampler.sampling_iteration()
-
-    assert len(events.alerts_queue) > 0
+    mocked_uptime.return_value = GRACE_TIME * 2
+    sampler.sampling_iteration()
+    assert len(events.alerts_queue) == 1
 
     all_alerts = list(events.alerts_queue.queue.queue)
-    assert all(alert == alerts.AlertCodes.VOLUME_LOW for alert in all_alerts)
+    assert all(alert == alerts.AlertCodes.PRESSURE_LOW for alert in all_alerts)
+
+
+@patch("data.alerts.uptime")
+@patch("time.time")
+@pytest.mark.parametrize('current_time', [-1 * GRACE_TIME, 10 * GRACE_TIME])
+def test_grace_time_alerts_move_time_forward(
+        mocked_time, mocked_uptime, events, config, sampler, current_time):
+    """Check no alerts in grace time, and alert received after.
+
+    Check that moving time after grace time doesn't cause alert in grace time.
+    Check that moving time before grace time doesn't prevent alert after grace time.
+    """
+    mocked_uptime.return_value = GRACE_TIME / 2
+    mocked_time.return_value = current_time
+
+    config.pressure_range = PressureRange(HIGH_THRESHOLD, HIGH_THRESHOLD)
+
+    sampler.sampling_iteration()
+    assert len(events.alerts_queue) == 0, "shouldn't get any alerts during grace time"
+
+    mocked_uptime.return_value = GRACE_TIME * 2
+    sampler.sampling_iteration()
+    assert len(events.alerts_queue) == 1
+
+    all_alerts = list(events.alerts_queue.queue.queue)
+    assert all(alert == alerts.AlertCodes.PRESSURE_LOW for alert in all_alerts)
