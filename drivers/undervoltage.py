1ac6a7d25e5e41ab67a4f9b0e8be90477f763ae1
Yonatan Schachter <yonatan.schachter@gmail.com>
2020-04-20 14:43:34 +0300

    fixed regex type mismatch
    Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>

--- drivers/undervoltage.py
+++ drivers/undervoltage.py
@@ -5,7 +5,7 @@ from subprocess import check_output
 class UnderVoltage(object):
     def read(self):
         output = check_output("/opt/vc/bin/vcgencmd get_throttled".split(" "))
-        status_register = re.findall("\d+$", output)[0]
+        status_register = re.findall(b"\d+$", output)[0]
         return int(status_register, 16) & 1
 
     def close(self):
