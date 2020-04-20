ae57df80b2dd3ee888b374c1783787880ebcfcd9
Yonatan Schachter <yonatan.schachter@gmail.com>
2020-04-20 14:31:38 +0300

    fixed matching of status register
    Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>

--- drivers/undervoltage.py
+++ drivers/undervoltage.py
@@ -1,10 +1,12 @@
+import re
 from subprocess import check_output
 
 
 class UnderVoltage(object):
     def read(self):
         output = check_output("/opt/vc/bin/vcgencmd get_throttled".split(" "))
-        return int(output[-6:-1], 16) & 1
+        status_register = re.findall("\d+$", output)[0]
+        return int(status_register, 16) & 1
 
     def close(self):
         pass
