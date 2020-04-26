823ea6812bec1ba37a61183d78527ec8696f6e15
rfire <rfire01@gmail.com>
2020-04-26 20:17:02 +0300

    plot tails + bug fix

--- plot.py
+++ plot.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from algo import VentilationStateMachine, VentilationState
 from data.events import Events
 from data.measurements import Measurements
-
+from drivers.mocks.sensor import DifferentialPressureMockSensor
+from logic.auto_calibration import TailDetector
 
 TIMESTAMP_COLUMN = "time elapsed (seconds)"
 FLOW_COLUMN = "flow"
@@ -26,6 +27,15 @@ def plot_file(file_path, start=0, end=-1):
     events = Events()
     vsm = VentilationStateMachine(measurements, events)
 
+    #  define auto calibrator
+    tail_detector = TailDetector(
+        dp_driver=DifferentialPressureMockSensor([0]),
+        sample_threshold=vsm._config.auto_cal_sample_threshold,
+        slope_threshold=vsm._config.auto_cal_slope_threshold,
+        min_tail_length=vsm._config.auto_cal_min_tail,
+        grace_length=vsm._config.auto_cal_grace_length
+    )
+
     # Run the machine
     for index, row in df.iterrows():
         vsm.update(
@@ -33,10 +43,14 @@ def plot_file(file_path, start=0, end=-1):
             flow_slm=row[FLOW_COLUMN],
             o2_percentage=row[OXYGEN_COLUMN],
             timestamp=row[TIMESTAMP_COLUMN])
+        tail_detector.add_sample(row[FLOW_COLUMN], row[TIMESTAMP_COLUMN])
 
+    tail_detector.process()
     fig, (ax_pressure, ax_flow) = plt.subplots(2, 1, sharex="all")
-    draw_pressure(ax_pressure, df[PRESSURE_COLUMN], df[TIMESTAMP_COLUMN], vsm)
-    draw_flow(ax_flow, df[FLOW_COLUMN], df[TIMESTAMP_COLUMN], vsm)
+    draw_pressure(ax_pressure, df[PRESSURE_COLUMN], df[TIMESTAMP_COLUMN], vsm,
+                  tail_detector)
+    draw_flow(ax_flow, df[FLOW_COLUMN], df[TIMESTAMP_COLUMN], vsm,
+              tail_detector)
 
     mng = plt.get_current_fig_manager()
     if hasattr(mng, "window"):
@@ -47,21 +61,19 @@ def plot_file(file_path, start=0, end=-1):
     plt.show()
 
 
-def draw_flow(axes, samples, timestamps, vsm):
+def draw_flow(axes, samples, timestamps, vsm, td):
     axes.set_ylabel("Air Flow (L/m)")
     axes.plot(timestamps, samples, "black")
-    axes.vlines(
-        vsm.entry_points_ts[VentilationState.PreInhale],
-        ymin=-40, ymax=40, colors="b", linestyle="dashed")
     axes.vlines(
         vsm.entry_points_ts[VentilationState.Inhale],
         ymin=-40, ymax=40, colors="g", linestyle="dashed")
     axes.vlines(
-        vsm.entry_points_ts[VentilationState.PreExhale],
-        ymin=-40, ymax=40, colors="pink", linestyle="dashed")
-    axes.vlines(
         vsm.entry_points_ts[VentilationState.Exhale],
         ymin=-40, ymax=40, colors="r", linestyle="dashed")
+    axes.vlines(td.start_tails_ts,
+                ymin=-40, ymax=40, colors="pink", linestyle="dotted")
+    axes.vlines(td.end_tails_ts,
+                ymin=-40, ymax=40, colors="brown", linestyle="dotted")
     axes.axhline(color="black", linewidth=1)
     where_insp = [s > 0 for s in samples]
     where_exp = [s < 0 for s in samples]
@@ -77,7 +89,7 @@ def draw_flow(axes, samples, timestamps, vsm):
         axes.annotate(f"{round(vol)}ml", (ts, -25), color="red")
 
 
-def draw_pressure(axes, samples, timestamp, vsm):
+def draw_pressure(axes, samples, timestamp, vsm, td):
     axes.set_ylabel("Pressure (cmH2O)")
     axes.plot(timestamp, samples, "black")
     axes.vlines(
