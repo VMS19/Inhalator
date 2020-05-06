d3cef0892612c18e3a0348207c64fe5fd107c85e
rfire <rfire01@gmail.com>
2020-05-06 13:40:22 +0300

    added test

--- tests/unit/test_auto_calibration.py
+++ tests/unit/test_auto_calibration.py
@@ -34,3 +34,34 @@ def test_single_cycle_tail_detection(offset):
 
     result = detector.process()
     assert result == pytest.approx(offset, rel=0.001)
+
+
+def test_single_cycle_tail_ts():
+    this_dir = os.path.dirname(__file__)
+    file_path = os.path.join(this_dir, SIMULATION_FOLDER,
+                             "single_step_cycle.csv")
+    driver_factory = DriverFactory(simulation_mode=True,
+                                   simulation_data=file_path)
+    dp_driver: DifferentialPressureMockSensor = driver_factory.acquire_driver("flow")
+    timer: MockTimer = driver_factory.acquire_driver("timer")
+    detector = TailDetector(dp_driver,
+                            sample_threshold=5,
+                            slope_threshold=10,
+                            min_tail_length=6,
+                            grace_length=5)
+
+    for _ in dp_driver.seq:
+        ts = timer.get_time()
+        flow_slm = dp_driver.read()
+
+        detector.add_sample(flow_slm, ts)
+
+    detector.process()
+    assert len(detector.start_tails_ts) == 1, "only one tail should be found"
+    assert 27.625 == detector.start_tails_ts[0], f"Expected tail to start 27.625, " \
+                                                 f"actually started at {detector.start_tails_ts[0]}"
+
+    assert len(detector.end_tails_ts) == 1, "only one tail should be found"
+    assert 27.967 == detector.end_tails_ts[0], f"Expected tail to start 27.625, " \
+                                               f"actually started at {detector.end_tails_ts[0]}"
+
