607c580e8f07bc06d5100c7b1c6f08faa349f4eb
Reznic <reznic.tal@gmail.com>
2020-05-07 09:52:18 +0300

    On unavailable measurement, we return the previously read value, instead of None, which will be 0

--- drivers/honeywell_pressure_sensor.py
+++ drivers/honeywell_pressure_sensor.py
@@ -63,10 +63,9 @@ class HoneywellPressureSensor(I2cDriver):
 
         elif status == self.STATUS_STALE_DATA:
             log.warning("Sensor's measure data not ready. sampling too fast?")
-            # raise UnavailableMeasurmentError("Sensor data unavailable")
-            """We don't raise error, so the read will return the 
-            previously read value.
-            Other possible behaviour that should be considered, is to 
+            """We don't raise UnavailableMeasurmentError, 
+            so the read will return the previously read value.
+            Another possible behaviour that should be considered, is to 
             retry reading, until new measurement is available (normal status).
             """
 
